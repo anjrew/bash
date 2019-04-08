3ddb446d797a033174da7a3b07da2297668294fb
earyzhe <earyzhe@gmail.com>
2019-04-08 15:38:35 +0200

    Ready with deploy now

--- web.sh
+++ web.sh
@@ -10,12 +10,22 @@ function create-files {
 function show-help {
   echo  "Functions";
 
-  echo  " - create-page";
+  echo  " - mkpg";
   echo  " - creates a directory file for a webpage with html,css and js files";
 
-  echo  " - create-files";
+  echo  " - mkfls";
   echo  " - creates html, css and js files within the current directory";
   
+  echo  " - deploy";
+  echo  " - Adds commits and pushs to the remote origin";
+}
+
+function git-deploy {
+    read -r -p 'Commit message: ' desc
+    git -C /Users/earyzhe/dev/bash add .
+    git -C /Users/earyzhe/dev/bash add -u
+    git -C /Users/earyzhe/dev/bash commit -m "$desc"
+    git -C /Users/earyzhe/dev/bash push origin HEAD:master
 }
 
 if [ $1 = mkpg ]
@@ -27,6 +37,9 @@ then
 elif [ $1 = help ]
 then
     show-help
+elif [ $1 = deploy ]
+then
+    git-deploy
 else
     echo "Unknown command"
 fi
