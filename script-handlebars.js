67c590001197b8c6e9868fbc1e7788ff17a5ba50
earyzhe <earyzhe@gmail.com>
2019-04-25 17:55:48 +0200

    Now has better feedback dynamic

---
+++ script-handlebars.js
@@ -0,0 +1,14 @@
+(function () {
+
+    Handlebars.templates = Handlebars.templates || {};
+
+    var templates = document.querySelectorAll('script[type="text/x-handlebars-template"]');
+
+    Array.prototype.slice.call(templates).forEach(function(script) {
+        Handlebars.templates[script.id] = Handlebars.compile(script.innerHTML);
+    });
+
+    // e.g
+    // document.getElementById('display').innerHTML = Handlebars.templates.cards({items: toRender});
+
+})();
